3f1c98beba534a9c7512505331ec458858658c3e
dimisjim <dimitris.moraitidis@gmail.com>
2018-11-27 00:17:52 +0200

    scala exec

--- src/main/scala/main_exec.scala
+++ src/main/scala/main_exec.scala
@@ -2,8 +2,18 @@ import scala.sys.process._
 
 object main_exec extends App {
 
-  val cmd = "ls -la"
+  val nbconvert = """"jupyter nbconvert --execute /notebook/notebooks/Untitled.ipynb""""
+  val nbconvertArray = Array[String]("jupyter", "nbconvert", "--execute", "/notebook/notebooks/Untitled.ipynb")
+//  val cmd = s"docker exec jupyter bash -c $nbconvert"
+//  val cmd = """docker exec jupyter bash -c "jupyter nbconvert --execute /notebook/notebooks/Untitled.ipynb""""
+//  val output = cmd.!!
+//  println(output)
+
+//  """docker exec jupyter bash -c "jupyter nbconvert --execute /notebook/notebooks/Untitled.ipynb"""" lineStream
+//  val cmd = Seq("docker", "exec", "jupyter", "bash", "-c", """"""", "jupyter", "nbconvert", "--execute", "/notebook/notebooks/Untitled.ipynb", """"""")
+//  cmd.lineStream
+
+  val cmd = "docker exec jupyter bash env"
   val output = cmd.!!
   println(output)
-
 }
